b1386e4900ae346625121ef569e7bef6462069f7
Edgar Damatian <damatian.e@gmail.com>
2024-02-14 09:21:20 -0800

    done

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => {
+    // Call the renderBooks function with the JSON object
+    renderBooks(json);
+  });  
+};   
+    // Call the renderBooks function with the books array
+    renderBooks(books);
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
+    // Find the 5th book in the series
+    const fifthBook = books[4];
+    console.log(fifthBook);
+
+    // Find the 103rd character in the series
+    const seriesText = books.reduce((text, book) => text + book.name, '');
+    const character = seriesText[1030];
+    console.log(character);
+
+    // Find the total number of pages of all the books
+    const totalPages = books.reduce((total, book) => total + book.numberOfPages, 0);
+    console.log(totalPages);
+
+    function renderBooks(books) {
+      const main = document.querySelector('main');
+      books.forEach(book => {
+        const h2 = document.createElement('h2');
+        h2.innerHTML = book.name;
+        main.appendChild(h2);
+      });
+    }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
